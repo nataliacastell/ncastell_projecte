cdf48563beff63f1c33cad398faa1f1216a871e1
ncastell <nataliacastell@iesmontsia.org>
2023-05-23 17:45:39 +0200

    migracions ok

---
+++ enxarxa/database/migrations/2023_05_23_123727_create_publicaciones_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePublicacionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publicaciones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->string('texto', 255);
+            $table->string('imagen', 255)->nullable();
+            $table->integer('likes')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publicaciones');
+    }
+}
